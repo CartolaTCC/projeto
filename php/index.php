29948eb0ef89d1ba150ef3f278d481f7296ea42d
Amandacra <amandabolattoancelmo@gmail.com>
2021-01-26 16:03:20 -0300

    alterando links

--- php/index.php
+++ php/index.php
@@ -7,8 +7,7 @@
 	<link rel="stylesheet" href="../css/w3.css">
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" />
 	<link rel="stylesheet" type="text/css" href="../css/estilo.css" />
-	<link rel="stylesheet" type="text/css" href="../css/perfil.css" />
-	<link rel="stylesheet" type="text/css" href="../css/cabecalho.css" />
+	<link rel="stylesheet" type="text/css" href="../css/login.css" />
 	<script src='../js/jquery-3.5.1.min.js'></script>
 	<script src="../js/bootstrap.min.js" ></script>
 	<link href = "https://fonts.googleapis.com/icon?family=Material+Icons" rel = "stylesheet"/>
