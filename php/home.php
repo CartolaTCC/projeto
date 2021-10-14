9d705acfea3728a04cf0dc760faa662e3cecbf42
= <=>
2021-10-14 18:53:01 -0300

    Adição das partidas reais

--- php/home.php
+++ php/home.php
@@ -3,105 +3,73 @@
 	//Estrutura head
 	head();
 
-  //Se o usuário estiver logado
-	if(isset($_SESSION["usuario"])){
-  	//Cabeçalho da página para usuários logados
-  	cabecalhoUsuarioLogado();
-  }else{ //Se não estiver logado
-		header('Location: index.php');
-  }
+	//Se o usuário estiver logado
+		if(isset($_SESSION["usuario"])){
+		//Cabeçalho da página para usuários logados
+		cabecalhoUsuarioLogado();
+	}else{ //Se não estiver logado
+			header('Location: index.php');
+	}
 ?>
+<script src='http://momentjs.com/downloads/moment.min.js'></script>
+<script>
+	$.ajax({
+		url: 'https://api.cartolafc.globo.com/rodadas',
+		timeout: 3000,
+		method: 'GET',
+		dataType: 'json',
+		success: function(dados){
+			for (var i = 0; i < 38; i++) {
+				if(moment().isBetween(dados[i].inicio, dados[i].fim)){
+					var rodada = dados[i].rodada_id;
+					var link = "https://api.cartolafc.globo.com/partidas/"
+					var urlAPI = link + rodada
 
+					$.ajax({
+						url: urlAPI,
+						timeout: 3000,
+						method: 'GET',
+						dataType: 'json',
+						success: function(dados){	
+							var HTML = "";
+							for (var i = 0; i < 10; i++) {																			
+								var mandanteID = dados.partidas[i].clube_casa_id;
+								var visitanteID = dados.partidas[i].clube_visitante_id;
+								
+								if(dados.partidas[i].placar_oficial_mandante){
+									HTML += '<div class = "jogo_container offset-1 col-10 jogo_card">';
+									HTML += '<div class = "infoJogo">';
+									HTML += '<p class = "text-center">'+ dados.partidas[i].local + '</p>';
+									HTML += '</div>';
+									HTML += '<div class = "row">';
+									HTML += '<div class = "col" style = "text-align: left; height: 100%; margin: auto 0;">';
+									HTML += '<img src = "'+ dados.clubes[mandanteID].escudos["60x60"] +'" alt="EscudoTime" class = "escudoTime"/>';
+									HTML += ''+ dados.clubes[mandanteID].nome +'';
+									HTML += '</div>';
+									HTML += '<div class = "col" style = "text-align: center;  height: 100%; margin: auto 0;">';
+									HTML += '<h1>'+ dados.partidas[i].placar_oficial_mandante +' <b>  X  </b>'+ dados.partidas[i].placar_oficial_visitante +'</h1>';				
+									HTML += '</div>';
+									HTML += '<div class = "col" style = "text-align: right;  height: 100%; margin: auto 0;">';
+									HTML += ''+ dados.clubes[visitanteID].nome +'';
+									HTML += '<img src = "'+ dados.clubes[visitanteID].escudos["60x60"] +'" alt="EscudoTime" class = "escudoTime"/>';
+									HTML += '</div>';
+									HTML += '</div>';
+									HTML += '<div>';
+									HTML += '<p class = "text-center infoJogo">'+ dados.partidas[i].partida_data +'</p>';
+									HTML += '</div>';
+									HTML += '</div> <br/>';
+									var result = document.getElementById("pagina").innerHTML = HTML
+								}							
+							}
+						}
+					});
+				}
+			}
+		}
+	});		 
+</script>
 <h1 class = "text-center pageTitle"><b>RODADAS DA SEMANA</b></h1>
-
-<div class = "container">
-
-	<!--Container do jogo (informações do time e dos resultado)-->
-	<div class = "jogo_container offset-1 col-10 jogo_card">
-
-		<!--Header do card: nome do estádio-->
-		<div class = "infoJogo">
-			<p class = "text-center">Estádio Vasco da Gama</p>
-		</div>
-
-		<!--Main do card: informações sobre a partida-->
-		<div class = "row">
-			<div class = "col" style = "text-align: left; height: 100%; margin: auto 0;">
-				<img src = "../img/escudos/fluminense.png" alt="EscudoTime" class = "escudoTime"/>
-				FLUMINENSE
-			</div>
-			<div class = "col" style = "text-align: center;  height: 100%; margin: auto 0;">
-				<h1>2 <b>X</b> 0</h1>
-			</div>
-			<div class = "col" style = "text-align: right;  height: 100%; margin: auto 0;">
-				BOTAFOGO
-				<img src = "../img/escudos/botafogo.png" alt="EscudoTime" class = "escudoTime"/>
-			</div>
-		</div>
-
-		<!--Footer do card: data do jogo-->
-		<div>
-			<p class = "text-center infoJogo">24/01/2020</p>
-		</div>
-	</div>
-
-	<!--Container do jogo (informações do time e dos resultado)-->
-	<div class = "jogo_container offset-1 col-10 jogo_card">
-
-		<!--Header do card: nome do estádio-->
-		<div class = "infoJogo">
-			<p class = "text-center">Estádio Antônio Accioly</p>
-		</div>
-
-		<!--Main do card: informações sobre a partida-->
-		<div class = "row">
-			<div class = "col" style = "text-align: left; height: 100%; margin: auto 0;">
-				<img src = "../img/escudos/atleticogo.png" alt="EscudoTime" class = "escudoTime"/>
-				ATLÉTICO-GO
-			</div>
-			<div class = "col" style = "text-align: center;  height: 100%; margin: auto 0;">
-				<h1>2 <b>X</b> 0</h1>
-			</div>
-			<div class = "col" style = "text-align: right;  height: 100%; margin: auto 0;">
-				FORTALEZA
-				<img src = "../img/escudos/fortaleza.png" alt="EscudoTime" class = "escudoTime"/>
-			</div>
-		</div>
-
-		<!--Footer do card: data do jogo-->
-		<div>
-			<p class = "text-center infoJogo">24/01/2020</p>
-		</div>
-	</div>
-
-	<!--Container do jogo (informações do time e dos resultado)-->
-	<div class = "jogo_container offset-1 col-10 jogo_card">
-
-		<!--Header do card: nome do estádio-->
-		<div class = "infoJogo">
-			<p class = "text-center">Adelmar da Costa Carvalho</p>
-		</div>
-
-		<!--Main do card: informações sobre a partida-->
-		<div class = "row">
-			<div class = "col" style = "text-align: left; height: 100%; margin: auto 0;">
-				<img src = "../img/escudos/sportrecife.png" alt="EscudoTime" class = "escudoTime"/>
-				SPORT RECIFE
-			</div>
-			<div class = "col" style = "text-align: center;  height: 100%; margin: auto 0;">
-				<h1>2 <b>X</b> 0</h1>
-			</div>
-			<div class = "col" style = "text-align: right;  height: 100%; margin: auto 0;">
-				BAHIA
-				<img src = "../img/escudos/bahia.png" alt="EscudoTime" class = "escudoTime"/>
-			</div>
-		</div>
-
-		<!--Footer do card: data do jogo-->
-		<div>
-			<p class = "text-center infoJogo">24/01/2020</p>
-		</div>
-	</div>
+<div id = "pagina" class = "container">
 </div>
 
 <?php
